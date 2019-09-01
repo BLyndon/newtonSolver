004beadd164d32c6d76a02717ae9d8cb297f3dac
Benjamin Heil <bheil@mail.de>
2019-09-01 23:45:40 +0200

    test system numerics

---
+++ test/coupledHarmonicOscillator.js
@@ -0,0 +1,44 @@
+/**
+ *  Test: two coupled harmonic oscillators
+ * 
+ *      n:          number of degrees of freedom
+ * 
+ *      state:      first column = coordinates
+ *                  second column = velocities
+ * 
+ *      parameter:  m1 = m2 = m = mass
+ *                  k1 = springs coupling to the fixed boundaries
+ *                  k2 = spring coupling the masses
+ */
+var n = 2
+var state = math.ones([n, 2]);
+var parameter = {
+    m: 1,
+    k1: 1,
+    k2: 1
+};
+var t = 0;
+
+state[0][0] = 1;
+state[0][1] = -1;
+
+state[1][0] = 0;
+state[1][1] = -1;
+
+function acc(state, t, parameter) {
+    let x = state[0][0];
+    let y = state[1][0];
+    let m = parameter.m;
+    let k1 = parameter.k1;
+    let k2 = parameter.k2;
+    let a = math.ones([n, 1]);
+    a[0][0] = (k2 * y - (k1 + k2) * x) / m;
+    a[1][0] = (k2 * x - (k1 + k2) * y) / m;
+    return a;
+}
+
+[state, elapsedTime] = solverRK(state, t, parameter, acc, 1000);
+
+console.log("state: (xi, vi)");
+console.log(state);
+console.log("elapsed time: " + elapsedTime + "s")
